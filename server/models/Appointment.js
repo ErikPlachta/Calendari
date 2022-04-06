985652db52c6618977fd62cad4f6b7b21acd802c
Mary Margaret <mmlawton15@gmail.com>
2022-04-06 10:26:54 -0400

    need help randomly generating strings for IDs and hardocded appointment types and statuses. should be good otherwise

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,75 +1,52 @@
-/*
-- ### Appointment
-    - All Existing Appointments
-    - Schema:
-        - id
-        - user_id
-        - business_id
-        - Appointment
-            - Obj containing appointment details
-            - Name
-            - Duration
-            - Date
-            - Start Time
-            - End Time
-            - Universal time-zone
-        - Client
-            - Obj containing client details
-            - Name
-            - Email
-            - Phone
-            - Summary
-            - timezone
-        - status
-            - Scheduled
-            - Completed
-            - Canceled
-*/
+/* All Existing Appointments */
 const {Schema, model} = require("mongoose");
 
 const AppointmentSchema = new Schema(
     {
         user_id: {
-            
+            type: String,
+            //randomly generate string
         },
         business_id: {
-
+            type: String,
+            //randomly generate string
         },
         appointment_details: {
             name: {
-                
+                //1 or 2 hardcoded examples here
             },
             duration: {
-
+                //1 hour and 2 hour?
             },
             date: {
-
             },
             start_time: {
-
             },
             end_time: {
-
             },
             //is universal time zone mvp?
         },
         client: {
             name: {
-
+                type: Schema.Type.name_first,
+                ref: 'User'
             },
             email: {
-
+                type: Schema.Type.email,
+                ref: 'User'
             },
             phone: {
-
+                type: Schema.Type.phone,
+                ref: 'User'
             },
             summary: {
-
+                type: Schema.Type.summary,
+                ref: 'User'
             },
             //is timezone mvp?
         },
         appointment_status: {
-            
+            //Scheduled, Completed, Cancelled
         },
         id: false
     }
