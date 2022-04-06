a7616f583881d8f9d46c991d6838ceeec5e2a1c5
Mary Margaret <mmlawton15@gmail.com>
2022-04-05 21:59:26 -0400

    what's the point of the business ID? got basic components laid out, also added client detials for the contact information. will revisit more tomorrow

--- server/models/Appointment_Type.js
+++ server/models/Appointment_Type.js
@@ -1,5 +1,4 @@
 /*
-
 - ### Appointment_Type
     - id
     - user_id
@@ -13,7 +12,29 @@
         - Duration
         - Date
         - Universal time-zone
+        */
+const {Schema, model} = require("mongoose");
 
+const Appointment_TypeSchema = new Schema(
+    {
+        user_id: {
+            type: Schema.Type.user_id,
+            ref: 'User'
+        },
+        //what is the point of the business id?
+        //i feel like the client model also needs to be included for the above information so going to include that!
+        client_details: {
+            type: Schema.Types.client,
+            ref: 'Appointment'
+        },
+        appointment_details: {
+            type: Schema.Types.appointment_details,
+            ref: 'Appointment'
+        },
+        id: false
+    }
+)
 
+const Appointment_Type = model('Appointment_Type', Appointment_TypeSchema);
 
-*/
+module.exports = Appointment_Type
