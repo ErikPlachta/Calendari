257d02a105b75960fcf71134d04126e5106889f3
Erik Plachta <eplachta@gmail.com>
2022-04-05 23:06:24 -0400

    Add hard-coded JSON for User and business draft

--- client/src/pages/Business.js
+++ client/src/pages/Business.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+//-- HARDCODED DATA
+var user =              require('../assets/json/user.json');
+var business =          require('../assets/json/business.json');
+var appointment =       require('../assets/json/appointment.json');
+var appointment_type =  require('../assets/json/appointment_type.json');
 
 export default function Business() {
+
+
+
   return (
     <div>Business</div>
   )
