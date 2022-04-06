35aec6061e3d0087794730c260dbf30f3806ff2c
Mary Margaret <mmlawton15@gmail.com>
2022-04-06 10:28:39 -0400

    need help randomly generating string ids, clheck out client detials, and need help on app date and duration, and mvp. should be good otherwise

--- server/models/Appointment_Type.js
+++ server/models/Appointment_Type.js
@@ -1,37 +1,42 @@
-/*
-- ### Appointment_Type
-    - id
-    - user_id
-    - business_id
-    - Appointment_Details
-        - Obj containing appointment details
-        - Name
-        - Email
-        - Phone
-        - Summary
-        - Duration
-        - Date
-        - Universal time-zone
-        */
+/*Appointment_Type */
+
 const {Schema, model} = require("mongoose");
 
 const Appointment_TypeSchema = new Schema(
     {
+        appointment_id: {
+            type: String,
+            //randomly generate string
+        },
+        business_id: {
+            type: String,
+            //reference business model
+        },
         user_id: {
             type: Schema.Type.user_id,
             ref: 'User'
         },
-        //what is the point of the business id?
-        //i feel like the client model also needs to be included for the above information so going to include that!
-        client_details: {
-            type: Schema.Types.client,
-            ref: 'Appointment'
+        client_details: {//this below format is probably incorrect but revisiting later with tutor!
+            type: Schema.Types.name_first,
+            ref: 'User',
+            type: Schema.Types.email,
+            ref:'User',
+            type: Schema.Types.phone,
+            ref: 'User'
         },
-        appointment_details: {
-            type: Schema.Types.appointment_details,
-            ref: 'Appointment'
+        summary: {
+            type: String,
+            trim: true,
+            length: 200,
         },
-        id: false
+        appointment_date: {
+            //help
+        },
+        appointment_duration: {
+            //help
+        },
+        //universal time zone mvp?
+
     }
 )
 
