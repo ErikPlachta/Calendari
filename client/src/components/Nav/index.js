f1c8aaa8766f2815f83d593aec2e2e91260dc75b
Mary Margaret <mmlawton15@gmail.com>
2022-04-06 20:42:09 -0400

    vanilla nav completed

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -10,10 +10,10 @@ export default function Nav(props) {
             <nav>
                 <ul>
                     <li>
-                        <a href="#signup">Signup</a>
+                        <a href="#signup" alt="signup">Signup</a>
                     </li>
                     <li>
-                        <a href="#signup">Login</a>
+                        <a href="#login" alt="login">Login</a>
                     </li>
                 </ul>
             </nav>
