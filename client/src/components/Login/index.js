64973914c83842e5332aa206b57ee1f1673f0607
Mary Margaret <mmlawton15@gmail.com>
2022-04-07 09:21:41 -0400

    removed comments and aadded validation for email and paassword. need to add code that verifies a user hasn't already been creted

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import {Link} from 'react-router'
+import { validateEmail } from '../../utils/helpers';
 import {capitalizeFirstLetter} from '../../utils/helpers';
 import Signup from "../Signup";
 
 export default function Login(props) {
   const {} = props;
 
+  const errorPopup = (e) => {
+    if (e.target.naame === 'email') {
+      const isEmail = validateEmail(e.target.value);
+      if (!isEmail) {
+        setErrorMessagae("Your email isn't valid");
+      } else {
+        setErrorMessagae('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessagae(`${e.target.password} is required`);
+      } else {
+        setErrorMessagae('')
+      }
+    }
+  }
+
   return (
       <section> 
           <h2 alt="please login">Please login to continue</h2>
           <div>
             <label htmlFor="email">Email:</label>
-            {/* need to add validation/handling below */}
-            <input type="text" name="email" defaultValue={email}/>
+            <input type="text" name="email" defaultValue={email} onBlur={errorPopup}/>
           </div>
           <div>
               <label htmlFor="password">Password:</label>
-              {/* need to add validation/handling below */}
-              <input type="password" name="password" defaultValue={password}/>
+              <input type="password" name="password" defaultValue={password} onBlur={errorPopup}/>
           </div>
           <div>
             <button type="submit">Login</button>
