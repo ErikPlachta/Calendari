d50e38fe1d4922ea5ee2aff1ba6c7818a0fb82dd
Mary Margaret <mmlawton15@gmail.com>
2022-04-07 09:19:18 -0400

    added validaation to the signup page

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router'
+import { validateEmail } from '../../utils/helpers';
 import {capitalizeFirstLetter} from '../../utils/helpers';
 import Login from "../Login";
 
 export default function Signup(props) {
   const {} = props;
+
+  const errorPopup = (e) => {
+    if (e.target.naame === 'email') {
+      const isEmail = validateEmail(e.target.value);
+      if (!isEmail) {
+        setErrorMessagae("Your email isn't valid");
+      } else {
+        setErrorMessagae('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessagae(`${e.target.password} is required`);
+      } else {
+        setErrorMessagae('')
+      }
+    }
+  }
 
   return (
     <section> 
@@ -12,12 +30,12 @@ export default function Signup(props) {
       <div>
         <label htmlFor="email">Email:</label>
         {/* need to add validation/handling below */}
-        <input type="text" name="email" defaultValue={email}/>
+        <input type="text" name="email" defaultValue={email} onBlur={errorPopup}/>
       </div>
       <div>
           <label htmlFor="password">Password:</label>
           {/* need to add validation/handling below */}
-          <input type="password" name="password" defaultValue={password}/>
+          <input type="password" name="password" defaultValue={password}  onBlur={errorPopup}/>
       </div>
       <div>
         <button type="submit">Sign Up</button>
