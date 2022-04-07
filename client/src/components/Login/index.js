9cefc7188f1452abfed5321b3dd0e0519bb9352e
Mary Margaret <mmlawton15@gmail.com>
2022-04-06 20:41:17 -0400

    finished basic login page

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import {capitalizeFirstLetter} from '../../utils/helpers';
+import Signup from "../Signup";
 
 export default function Login(props) {
   const {} = props;
 
   return (
-      <header> 
-          <h2 alt="appointment scheduler">Appointment Scheduler</h2>
-          <nav>
-              <ul>
-                  <li>
-                      <a href="#signup">Signup</a>
-                  </li>
-                  <li>
-                      <a href="#signup">Login</a>
-                  </li>
-              </ul>
-          </nav>
-      </header>
+      <section> 
+          <h2 alt="please login">Please login to continue</h2>
+          <div>
+            <label htmlFor="email">Email:</label>
+            {/* need to add validation/handling below */}
+            <input type="text" name="email" defaultValue={email}/>
+          </div>
+          <div>
+              <label htmlFor="password">Password:</label>
+              {/* need to add validation/handling below */}
+              <input type="password" name="password" defaultValue={password}/>
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+          <div>
+            {/* need to figure out how to make this button click redirect too the signup page */}
+            <p>Don't have an account? <a>Sign up</a> to continue</p>
+          </div>
+      </section>
   )
 }
