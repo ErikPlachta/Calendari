760b43e4077bf0723b8e83dfc073a2900ba48c40
Erik Plachta <eplachta@gmail.com>
2022-04-07 22:26:32 -0400

    Add App.js to contain all concept data

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,34 @@ import './assets/css/animations.css';
 
 
 import React from 'react';
+import Nav from './components/Nav';
 import Home from './pages/Home';
+
 import Business from './pages/Business';
 import Business_id from './pages/Business_id';
+import Schedule from './pages/Schedule';
 
 function App() {
   return (
     <section>
-      <h1>Your Schedule</h1>
+      <Nav></Nav>
     
       {/* <Home></Home> */}
       {/* hard-coded business into default load for easy testing */}
       <main>
+        <hr />
+        <hr />
+        <h2>SCHEDULE CONCEPT</h2>
+        <Nav></Nav>
+        <hr />
+        <hr />
+        <h2>BUSINESS DASHBOARD CONCEPT</h2>
+        <Business></Business>
+        <hr />
+        <hr />
+        <h2>BUSINESS SCHEDULER CONCEPT</h2>
         <Business_id></Business_id>
+        
         </main>
     </section>
   );
