c3cac63bcbd541a1d077ba3aff226d8cb80ced00
Mary Margaret <mmlawton15@gmail.com>
2022-04-07 21:00:15 -0400

    addd calendar and time chooser to schedule. isn't pretty but it's there '

--- client/src/pages/Schedule.js
+++ client/src/pages/Schedule.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import TimePicker from 'react-time-picker';
 
 export default function Schedule() {
+    const [value, onChange] = useState('9:00');
   return (
     <section>
         <Calendar>Pick your appointment date</Calendar>
+        <p>Please select your appointment time</p>
+        <TimePicker onChange={onChange} value={value}></TimePicker>
     </section>
     
   )
