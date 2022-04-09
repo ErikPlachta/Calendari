ed5daaedf7716729f3036a638b377c2d731c072e
Erik Plachta <eplachta@gmail.com>
2022-04-08 20:46:26 -0400

    Add apollo framework for jwt

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,27 @@ import calendarDynamic from './assets/svg/calender-dynamic-gradient.svg';
 import calendarIso from './assets/svg/calender-iso-gradient.svg';
 import calendarFront from './assets/svg/calender-front-gradient.svg';
 
-
 //-- PAGES
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Business from './pages/Business';
 import BusinessScheduler from './pages/BusinessScheduler';
 import Schedule from './pages/Schedule';
+
+
+import { setContext } from '@apollo/client/link/context';
+
+//-- AUTH TOKEN
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 
 function App() {
   return (
