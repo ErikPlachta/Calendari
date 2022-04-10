cb648a59a9ef60d9e24e5ec78dbd9d8d2a86b02d
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-04-10 14:45:58 -0400

    integrated apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import './assets/css/styles.css';
 import './assets/css/animations.css';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import React from 'react';
 import Nav from './components/Nav';
@@ -11,28 +13,51 @@ import Business from './pages/Business';
 import BusinessScheduler from './pages/BusinessScheduler';
 import Schedule from './pages/Schedule';
 
+const httpLink = createHttpLink({
+  // uri: '/graphql'
+  uri: 'http://localhost:3001/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <section>
-      <Nav></Nav>
-    
-      {/* <Home></Home> */}
-      {/* hard-coded business into default load for easy testing */}
-      <main>
-        <hr />
-        <hr />
-        <h2>BUSINESS DASHBOARD CONCEPT</h2>
-        <Business></Business>
-        <hr />
-        <hr />
-        <h2>BUSINESS SCHEDULER CONCEPT</h2>
-        <BusinessScheduler></BusinessScheduler>
-        <hr />
-        <hr />
-        <h2>SCHEDULE CONCEPT</h2>
-        <Schedule></Schedule>
+    <ApolloProvider client={client}>
+
+      <section>
+        <Nav></Nav>
+
+        {/* <Home></Home> */}
+        {/* hard-coded business into default load for easy testing */}
+        <main>
+          <hr />
+          <hr />
+          <h2>BUSINESS DASHBOARD CONCEPT</h2>
+          <Business></Business>
+          <hr />
+          <hr />
+          <h2>BUSINESS SCHEDULER CONCEPT</h2>
+          <BusinessScheduler></BusinessScheduler>
+          <hr />
+          <hr />
+          <h2>SCHEDULE CONCEPT</h2>
+          <Schedule></Schedule>
         </main>
-    </section>
+      </section>
+    </ApolloProvider>
   );
 }
 
