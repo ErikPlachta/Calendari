fa8de39a364a74453560fdd3039686c46690d6da
Mary Margaret <mmlawton15@gmail.com>
2022-04-11 18:58:59 -0400

    messing with waves box, need backup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,23 +8,22 @@ export default function Home() {
     <section>
       <div id="wavecontainer">
         <div id="calendariSlogan" class="slogan">
-          <div class="inner-header flex">
-            <h1>Your Schedule: Untangled</h1>
-          </div>
+            <h1 class="inner-header flex">Your Schedule: Untangled</h1>
+        </div>
           <div>
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
               <defs>
                 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
               </defs>
                 <g class="parallax">
-                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
                 <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
                 <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
                 <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
                 </g>
             </svg>
           </div>
-        </div>
+        
         <div class="content flex">
           <p>By Erik, Christiana and Mary Margaret</p>
         </div>
