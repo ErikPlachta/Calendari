67322cb6654a8ba50cbeae2964d6996483549f97
Mary Margaret <mmlawton15@gmail.com>
2022-04-11 09:34:00 -0400

    removed section of code for scheduling workday for business

--- client/src/pages/Business.js
+++ client/src/pages/Business.js
@@ -198,51 +198,10 @@ export default function Business() {
                 </div>
               </section>
             ))}
-            
-            
-          </div>
 
-        </section>
-        
-        {/* THIS SHOULD BE FUTURE DEVELOPMENT */}
-        <section className="containerResults dayOfWeek">
-            {/* Setup Days of Week, section. */}
-            <h3>It looks like your schedule needs to be setup!</h3>
-            
-            <div className='dayOfWeek'>
-                <p>Please confirm the days and times you are available for appointments.</p>
-                <form className='dayOfWeek'>
-                    {Object.keys(schedule).map( (dayOfWeek, index) => (
-                    <div>
-                        
-                        <h4>{capitalizeFirstLetter(dayOfWeek)}</h4>
-                        {/* Go through each day of week, present days with times and if verified */}
-                        {Object.keys(schedule[dayOfWeek]).map((value, index) => ( 
-                            <span>
-                            {(() => {
-                                switch (value) {
-                                    case 'start'    :   return  <input type='time' id={(`${dayOfWeek}_start`)} defaultValue={schedule[dayOfWeek][value]}></input>;
-                                    case 'end'      :   return  <input type='time' id={(`${dayOfWeek}_end`)} defaultValue={schedule[dayOfWeek][value]}></input>;
-                                    case 'verified' :   return  <input type="checkbox" id={(`${dayOfWeek}_verified`)} />;
-                                    // checked={checked ? 'checked' : ''}
-                                    default         :   return "NULL";
-                                }
-                                })()}
-                            {/* { (`${schedule[dayOfWeek][time]} -`)  || schedule[dayOfWeek][time] } */}
-                            </span>
-                        ))}
-                    </div>
-                    ))}
-                    {/* submit button for times */}
-                    <input type='button' value="Approve Times" onClick={approveTimes}></input>
-                </form>
             </div>
         </section>
-        
-        
-
       </main>
-
     </section>
   )
 }
