854454fc9a9d7356e2c5561379f9a7730befbc26
Erik Plachta <eplachta@gmail.com>
2022-04-11 11:48:39 -0400

    Server/Seeder/seeds.js - Created function seedRandomData() to seed non-connected database data with Faker. Adding framework in function  seedExplicitData() for a business specific seed that we can use for the next level of testing.

--- server/models/Appointment_Type.js
+++ server/models/Appointment_Type.js
@@ -1,13 +1,14 @@
 const { Schema, model } = require("mongoose");
 
 
+//-- Example of an Appointment Type associated to a business
 const example =
 {
     "_id"           : "0000-0001",
     "name"          :   "Non-Federation Citizen - Starfleet Exam 🌎",
     "summary"       :   "For non-Federation citizens, eligibility is considered with a letter of reference from a Command Level Starfleet Officer on file.",
     "description"   :   "This exam is designed to look for certain qualities a person displayed, including: integrity, intelligence, courage, imagination, and leadership qualities. Candidates are to expect the unexpected during this exam.",
-    "date_time"     :   "6 Hours",
+    "date_time"     :   "TBD",
     "Details"       :   {
                         "subject"       : "Non-Federation Citizen - Starfleet Exam",
                         "date"          : "04/23/2022",
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -3,210 +3,320 @@ const faker = require('faker'); /* https://fakerjs.dev/api/ */
 const db = require('../config/connection');
 const { User, Business, Appointment, Appointment_Type } = require('../models');
 
-//-- Make db connection, delete ALL content, and build clean seed data.
-db.once('open', async () => {
-    
-    //-- Purge local data to reset
-    await User.deleteMany({});
-    await Business.deleteMany({});
-    await Appointment.deleteMany({});
-    await Appointment_Type.deleteMany({});
-    
 
 
-    /*    USER
-
-            - Creates 10 users with randomized info with accounts that are
-                up to 1-year old.
-            - Adds to database
-    */
-
-    const userData = [];
-   
-    //-- Create 10 Users Data    
-    for (let i = 0; i < 10; i += 1) {
-        const name_first = faker.name.firstName();
-        const name_last = faker.name.lastName();
-        const username = faker.internet.userName();
-        const email = faker.internet.exampleEmail(username); /* using example to ensure not real */
-        const password = faker.internet.password();
-        const date_created = faker.date.past(1); /* date in last 1 year */
-        userData.push({ name_first, name_last, username, email, password, date_created });
-    }
-
-    //-- Add created users to database
-    const createdUsers = await User.collection.insertMany(userData);
-
-
-    console.log("##-- Created Users complete.")
-    console.table(userData)
-
-
-
-
-
-     /*    APPOINTMENT_TYPE
-
-            - Creates a basic appointment type
-            - Add to database
-    */
-
-    const appointmentTypeData = [];
-
-    //-- Create unique Business
-    for (let i = 0; i < 1; i += 1) {
+function seedExplicitData(){
+    db.once('open', async () => {
         
-        
-        const name = "General";
-        const description =  "Schedule an appointment.";
-        const date_time =  "";
-        //-- template of what it needs to be, should be empty here.
-        const Details = {
-            "subject"       : "",
-            "date_time"          : "",
-            "duration"      : "",
-            "timezone"      : "",
-            "client"        : {
-                                "name": "",
-                                "email": "",
-                                "phone": ""
+        //-- Purge local data to reset
+        await User.deleteMany({});
+        await Business.deleteMany({});
+        await Appointment.deleteMany({});
+        await Appointment_Type.deleteMany({});
+    
+
+        //-- seeder for StarFleet Users
+        const userData = [
+            {
+                "_id"           :   "0000-0000",
+                "name_first"    :   "Erik",
+                "name_Last"     :   "Plachta",
+                "username"      :   "Erik",
+                "password_has"  :   "abcdefg-heres-my-password-don't-hack-me",
+                "date_created"  :   "1649213599934",
+                "date_login"    :   "1649213599934",
+                "Appointments"  :   [
+                                        "0000-0000-1111"
+                ],
+                "business_id"   :   "0000-AAAA"
             },
-            "configuration" : {}
-        };
-        // const Appointments = {};
-        // const Users = {};
-
-        //-- build array
-        appointmentTypeData.push({ name, description, date_time, Details });
-    }
-
-    //-- Add created appointment_type to database
-    const createdAppointmentType = await Appointment_Type.collection.insertMany(appointmentTypeData);
-
-
-    console.log("##-- Created Appointment_Type complete.")
-    console.table(appointmentTypeData)
-
-
-
-
-
-faker.date.recent(10)
-    /*-- APPOINTMENT  --------------------------------------------------------*/
-
-    const appointmentData = [];
-
-    //-- Create unique Business
-    for (let i = 0; i < 50; i += 1) {
-        
-        //-- used for username and email below, not sent directly
-        const name = faker.name.findName();
-
-        const status = "scheduled";
-        const date_created = faker.date.recent(10);
-        const Details = {
-            "subject"       : faker.lorem.words(Math.round(Math.random() * 10) + 1),
-            "duration"      : "45 minutes",
-            "date_time"    : faker.date.between('2022-04-10T00:00:00.000Z', '2022-05-01T00:00:00.000Z'),
-            "timezone"      : "EST",
-            "client"        : {
-                                "name": name,
-                                "email": faker.internet.exampleEmail(name), /* using example to ensure not real */
-                                "phone": faker.phone.phoneNumber('###-###-###')
+            {
+                "_id"           :   "1111-1111",
+                "name_first"    :   "Christiana",
+                "name_Last"     :   "Sullivan Morales",
+                "username"      :   "Christiana",
+                "password_has"  :   "abcdefg-heres-my-password-don't-hack-me",
+                "date_created"  :   "1649213599934",
+                "date_login"    :   "1649213599934",
+                "appointments"  :   [
+                                        "0000-0000-2222"
+                ],
+                "business_id"   :   "0000-AAAA"
+            },
+            {
+                "_id"           :   "2222-2222",
+                "name_first"    :   "Mary",
+                "name_Last"     :   "Lawton",
+                "username"      :   "Mary",
+                "password_has"  :   "abcdefg-heres-my-password-don't-hack-me",
+                "date_created"  :   "1649213599934",
+                "date_login"    :   "1649213599934",
+                "appointments"  :   [
+                                        "0000-0000-3333"
+                ],
+                "business_id"   :   "0000-AAAA"
             }
-        };
-        // const Appointments = {};
-        // const Users = {};
+            
+        ];
 
-        //-- build array
-        appointmentData.push({ status, date_created, Details });
-    }
-
-    //-- Add created appointment_type to database
-    const createdAppointmentData = await Appointment.collection.insertMany(appointmentData);
-
-    console.log("##-- Created Appointment Data complete.")
-    console.table(appointmentData)
+        //-- Add created users to database
+        const createdUsers = await User.collection.insertMany(userData);
 
 
-
-
-
-
-    /*    BUSINESS
-
-            - Creates X businesses
-            - Relate to Users
-            - Add to Database
-            - Adds to database
-
-    */
-
-    const businessData = [];
-
-    //-- Create unique Business
-    for (let i = 0; i < 10; i += 1) {
-        const name = faker.company.companyName();
-        const brand_name = name.replace(/\s+/g, ''); /* take name and make it url friendly */
-        const configuration = {
-            "schedule"  : {
-                "sunday": {
-                    "start"     :   "00:00",
-                    "end"       :   "00:00",
-                    "verified"  :   "false"
-                },
-                "monday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
-                },
-                "tuesday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
-                },
-                "wednesday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
-                },
-                "thursday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
-                },
-                "friday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
-                },
-                "saturday": {
-                    "start"     :   "09:00",
-                    "end"       :   "15:00",
-                    "verified"  :   "false"
+        //-- seeder for StarFleet Appointments
+        const appointmentTypeData = [
+            {
+                "_id"           :   "0000-0000",
+                "name"          :   "Federation Citizen - Starfleet Exam 🖖🏼",
+                "summary"       :   "All Federation citizens, schedule your StarFleet Exam here.",
+                "description"   :   "This exam is designed to look for certain qualities a person displayed, including: integrity, intelligence, courage, imagination, and leadership qualities. Candidates are to expect the unexpected during this exam.",
+                "date_time"          :   "6 Hours",
+                "Details"        :   {
+                                        "subject"       : "Federation Citizen - Starfleet Exam",
+                                        "date"          : "04/14/2022",
+                                        "duration"      : "Undetermined",
+                                        "time_start"    : "04:00",
+                                        "timezone"      : "Earth",
+                                        "client"        : {
+                                                            "name": "",
+                                                            "email": "",
+                                                            "phone": ""
+                                        }
+                }
+            },
+            {
+                "_id"           :   "0000-0001",
+                "name"          :   "Non-Federation Citizen - Starfleet Exam 🌎",
+                "summary"       :   "For non-Federation citizens, eligibility is considered with a letter of reference from a Command Level Starfleet Officer on file.",
+                "description"   :   "This exam is designed to look for certain qualities a person displayed, including: integrity, intelligence, courage, imagination, and leadership qualities. Candidates are to expect the unexpected during this exam.",
+                "date_time"          :   "6 Hours",
+                "Details"        :   {
+                                        "subject"       : "Non-Federation Citizen - Starfleet Exam",
+                                        "date"          : "04/23/2022",
+                                        "duration"      : "Undetermined",
+                                        "time_start"    : "04:00",
+                                        "timezone"      : "Earth",
+                                        "client"        : {
+                                                            "name": "",
+                                                            "email": "",
+                                                            "phone": ""
+                                        }
                 }
             }
-        };
-        const Appointment_Types = {};
-        const Appointments = {};
-        const Users = {};
+        ]
+         
+
+    });
+};
+
+
+function seedUniqueData(){
+
+    //-- Make db connection, delete ALL content, and build clean seed data.
+    db.once('open', async () => {
         
-        businessData.push({ name, brand_name, configuration});
-    }
+        //-- Purge local data to reset
+        await User.deleteMany({});
+        await Business.deleteMany({});
+        await Appointment.deleteMany({});
+        await Appointment_Type.deleteMany({});
+        
 
-    //-- Add created users to database
-    const createdBusinesses = await Business.collection.insertMany(businessData);
 
-    console.log("##-- Created Businesses complete.")
-    console.table(businessData)
+        /*    USER
+
+                - Creates 10 users with randomized info with accounts that are
+                    up to 1-year old.
+                - Adds to database
+        */
+
+        const userData = [];
     
+        //-- Create 10 Users Data    
+        for (let i = 0; i < 10; i += 1) {
+            const name_first = faker.name.firstName();
+            const name_last = faker.name.lastName();
+            const username = faker.internet.userName();
+            const email = faker.internet.exampleEmail(username); /* using example to ensure not real */
+            const password = faker.internet.password();
+            const date_created = faker.date.past(1); /* date in last 1 year */
+            userData.push({ name_first, name_last, username, email, password, date_created });
+        }
 
-    //TODO:: 04/06/2022 #EP || Add relationships. ( basic concept below for future. )
-    // await Business.updateOne({ _id: userId }, { $addToSet: { Users: userId } });
-    // const Users = createdUser[createdUsers.length-1];
-    // createdUsers.pop(); /* remove the last user from array after assigning it to business*/
+        //-- Add created users to database
+        const createdUsers = await User.collection.insertMany(userData);
 
 
-    console.log('//-- No relational seeding is completed');
-    process.exit(0);
-});
+        console.log("##-- Created Users complete.")
+        console.table(userData)
+
+
+
+
+
+        /*    APPOINTMENT_TYPE
+
+                - Creates a basic appointment type
+                - Add to database
+        */
+
+        const appointmentTypeData = [];
+
+        //-- Create unique Business
+        for (let i = 0; i < 1; i += 1) {
+            
+            
+            const name = "General";
+            const description =  "Schedule an appointment.";
+            const date_time =  "";
+            //-- template of what it needs to be, should be empty here.
+            const Details = {
+                "subject"       : "",
+                "date_time"          : "",
+                "duration"      : "",
+                "timezone"      : "",
+                "client"        : {
+                                    "name": "",
+                                    "email": "",
+                                    "phone": ""
+                },
+                "configuration" : {}
+            };
+            // const Appointments = {};
+            // const Users = {};
+
+            //-- build array
+            appointmentTypeData.push({ name, description, date_time, Details });
+        }
+
+        //-- Add created appointment_type to database
+        const createdAppointmentType = await Appointment_Type.collection.insertMany(appointmentTypeData);
+
+
+        console.log("##-- Created Appointment_Type complete.")
+        console.table(appointmentTypeData)
+
+
+
+
+
+    faker.date.recent(10)
+        /*-- APPOINTMENT  --------------------------------------------------------*/
+
+        const appointmentData = [];
+
+        //-- Create unique Business
+        for (let i = 0; i < 50; i += 1) {
+            
+            //-- used for username and email below, not sent directly
+            const name = faker.name.findName();
+
+            const status = "scheduled";
+            const date_created = faker.date.recent(10);
+            const Details = {
+                "subject"       : faker.lorem.words(Math.round(Math.random() * 10) + 1),
+                "duration"      : "45 minutes",
+                "date_time"    : faker.date.between('2022-04-10T00:00:00.000Z', '2022-05-01T00:00:00.000Z'),
+                "timezone"      : "EST",
+                "client"        : {
+                                    "name": name,
+                                    "email": faker.internet.exampleEmail(name), /* using example to ensure not real */
+                                    "phone": faker.phone.phoneNumber('###-###-###')
+                }
+            };
+            // const Appointments = {};
+            // const Users = {};
+
+            //-- build array
+            appointmentData.push({ status, date_created, Details });
+        }
+
+        //-- Add created appointment_type to database
+        const createdAppointmentData = await Appointment.collection.insertMany(appointmentData);
+
+        console.log("##-- Created Appointment Data complete.")
+        console.table(appointmentData)
+
+
+
+
+
+
+        /*    BUSINESS
+
+                - Creates X businesses
+                - Relate to Users
+                - Add to Database
+                - Adds to database
+
+        */
+
+        const businessData = [];
+
+        //-- Create unique Business
+        for (let i = 0; i < 10; i += 1) {
+            const name = faker.company.companyName();
+            const brand_name = name.replace(/\s+/g, ''); /* take name and make it url friendly */
+            const configuration = {
+                "schedule"  : {
+                    "sunday": {
+                        "start"     :   "00:00",
+                        "end"       :   "00:00",
+                        "verified"  :   "false"
+                    },
+                    "monday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    },
+                    "tuesday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    },
+                    "wednesday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    },
+                    "thursday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    },
+                    "friday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    },
+                    "saturday": {
+                        "start"     :   "09:00",
+                        "end"       :   "15:00",
+                        "verified"  :   "false"
+                    }
+                }
+            };
+            const Appointment_Types = {};
+            const Appointments = {};
+            const Users = {};
+            
+            businessData.push({ name, brand_name, configuration});
+        }
+
+        //-- Add created users to database
+        const createdBusinesses = await Business.collection.insertMany(businessData);
+
+        console.log("##-- Created Businesses complete.")
+        console.table(businessData)
+        
+
+        //TODO:: 04/06/2022 #EP || Add relationships. ( basic concept below for future. )
+        // await Business.updateOne({ _id: userId }, { $addToSet: { Users: userId } });
+        // const Users = createdUser[createdUsers.length-1];
+        // createdUsers.pop(); /* remove the last user from array after assigning it to business*/
+
+
+        console.log('//-- No relational seeding is completed');
+        process.exit(0);
+    });
+};
