f9031fb1ca2a509cb9a678fcdde31bcd2444e190
Mary Margaret <mmlawton15@gmail.com>
2022-04-12 09:44:14 -0400

    adding padding-0 to wave container

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
     <section>
       <div id="wavecontainer">
           <div class="slogan">
-          <h3 id="sloganForHero" class="inner-header flex">Your Schedule: Untangled</h3>
+            <h3 id="sloganForHero" class="inner-header flex">Your Schedule: Untangled</h3>
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
               <defs>
                 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
