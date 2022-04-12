4de6e9a80eaff428d8819d6d287411404ea1f770
Mary Margaret <mmlawton15@gmail.com>
2022-04-11 20:26:46 -0400

    working on the styling, got the wave to move and working on the footer:

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,10 +7,9 @@ export default function Footer(props) {
     <footer classname="footer">
       <div class="footerElements">
         <img alt="Bob" src={bob1} width="50px"></img>
-      </div>
-      <div class="footerElements">
         <p>Copyright of Mary Margaret Lawton, Erik Plachta, and Christiana Sullivan Morales</p> 
       </div>
+        
     </footer>
 
   )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <section>
       <div id="wavecontainer">
-        <div id="calendariSlogan" class="slogan">
-            <h1 class="inner-header flex">Your Schedule: Untangled</h1>
+        <div id="calendariSlogan">
+            <h3 id="sloganForHero" class="inner-header flex">Your Schedule: Untangled</h3>
         </div>
-          <div>
+          <div class="slogan">
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
               <defs>
                 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
