3f56633eb2d5750f570f309fbb369f088bead031
Erik Plachta <eplachta@gmail.com>
2022-04-12 15:33:15 -0400

    Rebuild with Heroku Server requirements regarding server.js

--- server/server.js
+++ server/server.js
@@ -28,20 +28,36 @@ const startServer = async () => {
   server.applyMiddleware({ app });
 
   // log where we can go to test our GQL API
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  console.log(`Server started. GraphQL middleware setup at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 // Initialize the Apollo server
+console.log("Starting server...")
 startServer();
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+//-- SETUP TO RUN FOR DEPLOYMENT SPECIFICALLY
+
+const path = require("path"); // Accessing the path module
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 
 // if we're in production, serve client/build as static assets
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
+console.log("Starting connection to database...")
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+console.log("SUCCESS: connection to database!")
