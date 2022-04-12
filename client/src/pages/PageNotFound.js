d5d8db144da306281246d0f8d6e1c59d4070bb8e
Mary Margaret <mmlawton15@gmail.com>
2022-04-12 18:34:09 -0400

    added styling to page not found

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import bob_static from '../assets/svg/bob_static.svg';
 
 export default function PageNotFound() {
-
   return (
     <main>
-          <div classname="errorPageElements">
+          <div class="signupInformation">
+            <img alt="Bob" src={bob_static} width="80px"></img>
             <h2>404 - Page Not Found</h2>
           </div>
     </main> 
