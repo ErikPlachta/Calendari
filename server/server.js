bb8226852fda9094607ea0d35b54a64ab1320b7f
Erik Plachta <eplachta@gmail.com>
2022-04-12 15:37:11 -0400

    Add updated pathing in the server.js to go to proper client folder!

--- server/server.js
+++ server/server.js
@@ -44,10 +44,10 @@ app.use(express.json());
 
 const path = require("path"); // Accessing the path module
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, '../client/build', "index.html"));
 });
 
 
