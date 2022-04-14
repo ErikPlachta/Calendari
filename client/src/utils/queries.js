1bfc31fb4a8d0aceafb6c76d786882d23e0b2183
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-04-13 20:15:57 -0400

    query business by id added to typeDefs, resolvers, and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,7 +18,6 @@ export const QUERY_BUSINESS_CLIENTS = gql`
             _id
             name
             brand_name
-            configuration
             users {
                 _id
                 name_first
@@ -42,7 +41,6 @@ export const QUERY_BUSINESS = gql`
             _id
             name
             brand_name
-            configuration
             appointments {
                 _id
                 appt_type_name
@@ -71,14 +69,13 @@ export const QUERY_BUSINESS = gql`
         }
     }
 `;
-// query ALL business info, including appt types, appts, and users
+// query ALL business info, including appt types, appts, and users - by brand name
 export const QUERY_BUSINESS_THOROUGH = gql`
     query Business($brandName: String!) {
         business(brand_name: $brandName) {
             _id
             name
             brand_name
-            configuration
             users {
                 _id
                 business_id
@@ -130,7 +127,67 @@ export const QUERY_BUSINESS_THOROUGH = gql`
             }
         }
     }  
-`
+`;
+// query ALL business info, including appt types, appts, and users - by id
+export const QUERY_BUSINESS_THOROUGH_BY_ID = gql`
+    query BusinessById($id: ID!) {
+        businessById(_id: $id) {
+        _id
+        name
+        brand_name
+        users {
+            _id
+            business_id
+            name_first
+            name_last
+            username
+            email
+            phone_number
+            date_created
+            appointments {
+                _id
+                appt_type_name
+                appt_type_summary
+                appt_type_notes
+                appointment_date
+                appointment_time
+                appointment_status
+                timezone
+                client_full_name
+                client_email
+                client_phone
+                appt_notes
+            }
+        }
+        appointment_types {
+            _id
+            appt_type_name
+            subject
+            summary
+            appointment_duration
+            appt_type_notes
+            appt_fields {
+                _id
+                field_name
+            }
+        }
+        appointments {
+            _id
+            appt_type_name
+            appt_type_summary
+            appt_type_notes
+            appointment_date
+            appointment_time
+            appointment_status
+            timezone
+            client_full_name
+            client_email
+            client_phone
+            appt_notes
+        }
+        }
+    }
+`;
 // query single user
 export const QUERY_USER = gql`
     query User($username: String!) {
