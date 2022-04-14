ff8f44b4dd0f529d8211c9c8098930ff008012c3
Erik Plachta <eplachta@gmail.com>
2022-04-14 00:19:50 -0400

    Remove shortcuts in nav. Unify styling to brand. On-click to homepage on logo.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,10 +6,8 @@ import { Link } from 'react-router-dom';
 export default function Nav({ bobData, bob2 }) {
 
     //--------------------------------------------------------------------------
-    //-- STATES
-
-    //TODO:: 04/13/22 #EP|| Delete?
-    const [bobs, setBobs] = useState({bobData});//-- Path to BOB SVG Icons
+    //-- ASSETS
+    const [bobs, setBobs] = useState({bobData});//-- Path to BOB SVG Icons //TODO:: 04/13/22 #EP|| Delete?
 
     //--------------------------------------------------------------------------
     //-- FUNCTIONS
@@ -23,18 +21,24 @@ export default function Nav({ bobData, bob2 }) {
     return (
         <header className="navHeader"> 
             <div>  
-                <img className="brandLogo" alt="Bob" src={bob2} width="50px"></img>
-                <span className="brandNameSlogan">
+                
+                <Link to="/">
+                    <img className="brandLogo" alt="Bob" src={bob2} width="50px"></img>
+                </Link>
+                <Link to="/">
+                    <span className="brandNameSlogan">
                     <h2 className="brandName" alt="appointment scheduler">
                         Calendari
                     </h2>
                 </span>
+                </Link>
             </div>
             {/* NAVIGATION */}
             <nav>
                 <ul>
                     {/*TODO:: 04/11/22 #EP || Add about pages  */}
                     {Auth.isLoggedIn() 
+                        
                         //-- If logged in, show this
                         ? (<>
                             <li> <a href="/">Calendari</a> </li>
@@ -42,8 +46,9 @@ export default function Nav({ bobData, bob2 }) {
                             <li> <Link to="/business">My Scheduler</Link></li>
                             <li><a href="/" onClick={logout}> Logout </a> </li>
                             {/*TODO:: 04/11/22 #EP || Add Support page for if need help  */}
-                            <li>Support</li>
+                            {/* <li>Support</li> */}
                         </>)
+                        
                         //-- If not logged in, show this
                         : (<>
                             <li> <a href="/#whatIsThis">Calendari</a> </li>
@@ -51,19 +56,10 @@ export default function Nav({ bobData, bob2 }) {
                             <li> <a href="/#freeVsPremium">Features</a> </li>
                             <li> <Link to="/login">Login</Link> </li>
                             <li> <Link to="/signup">Signup</Link> </li>
-                        </>)
-                        
-                    }
-                    
+                        </>)   
+                    }  
                 </ul>
             </nav>
-            
-            {/*TODO:: 04/11/22 #EP|| Remove once done testing  */}
-            {/* <div style={{position: "absolute", top:"0", right: "0", display: "flex", margin: "auto", flexDirection: "row", justifyContent:"space-evenly", border: "2px solid red" }}>
-                <h5>Placeholder Links for Testing 👉🏼</h5>
-                <a href="/s/0000-AAAA" alt="scheduler">schedule/0000-AAAA</a>
-                <a href="/b/0000-AAAA" alt="business">business/0000-AAAA</a>
-            </div> */}
         </header>
     )
 };
