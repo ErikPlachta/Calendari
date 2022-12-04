413546941bcfcb2ca0df99d7d20df0b47947ad6b
Erik Plachta <eplachta@gmail.com>
2022-12-04 12:14:12 -0500

    Copyright and yr to match.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default function Footer({bobData, bob2}) {
       <div className="footerElements">
         <img className="brandLogo"alt="Bob" src={bob2} width="50px"></img>
         <p>
-          By Christiana Sullivan Morales, Erik Plachta, & Mary Margaret Lawton |  <b>&#169; 2022</b> 
+          By Christiana Sullivan Morales, Erik Plachta, & Mary Margaret Lawton |  <b>&#169;2022</b> 
           
         </p>
       </div>
